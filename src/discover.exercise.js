65690afd9cd017966c00a35871be46bd00fb71d4
Oliver Day <oliver.day.swe@gmail.com>
2023-01-23 19:40:26 -0800

    feat: add http requests solution

--- src/discover.exercise.js
+++ src/discover.exercise.js
@@ -1,41 +1,50 @@
 /** @jsx jsx */
-import {jsx} from '@emotion/core'
-
-import './bootstrap'
-import Tooltip from '@reach/tooltip'
-import {FaSearch} from 'react-icons/fa'
-import {Input, BookListUL, Spinner} from './components/lib'
-import {BookRow} from './components/book-row'
-// 🐨 import the client from './utils/api-client'
+import { jsx } from '@emotion/core';
+import * as React from 'react';
+import './bootstrap';
+import Tooltip from '@reach/tooltip';
+import { FaSearch, FaTimes } from 'react-icons/fa';
+import { Input, BookListUL, Spinner } from './components/lib';
+import { BookRow } from './components/book-row';
+import { client } from './utils/api-client';
+import * as colors from './styles/colors';
 
 function DiscoverBooksScreen() {
-  // 🐨 add state for status ('idle', 'loading', or 'success'), data, and query
-  const data = null // 💣 remove this, it's just here so the example doesn't explode
-  // 🐨 you'll also notice that we don't want to run the search until the
-  // user has submitted the form, so you'll need a boolean for that as well
-  // 💰 I called it "queried"
+  const [query, setQuery] = React.useState('');
+  const [queried, setQueried] = React.useState(false);
+  const [status, setStatus] = React.useState('idle');
+  const [data, setData] = React.useState(null);
+  const [error, setError] = React.useState(null);
 
-  // 🐨 Add a useEffect callback here for making the request with the
-  // client and updating the status and data.
-  // 💰 Here's the endpoint you'll call: `books?query=${encodeURIComponent(query)}`
-  // 🐨 remember, effect callbacks are called on the initial render too
-  // so you'll want to check if the user has submitted the form yet and if
-  // they haven't then return early (💰 this is what the queried state is for).
+  const isLoading = status === 'loading';
+  const isSuccess = status === 'success';
+  const isError = status === 'error';
 
-  // 🐨 replace these with derived state values based on the status.
-  const isLoading = false
-  const isSuccess = false
+  React.useEffect(() => {
+    if (!queried) {
+      return;
+    }
+    setStatus('loading');
+    client(`books?query=${encodeURIComponent(query)}`)
+      .then(responseData => {
+        setData(responseData);
+        setStatus('success');
+      })
+      .catch(error => {
+        setError(error);
+        setStatus('error');
+      });
+  }, [query, queried]);
 
   function handleSearchSubmit(event) {
-    // 🐨 call preventDefault on the event so you don't get a full page reload
-    // 🐨 set the queried state to true
-    // 🐨 set the query value which you can get from event.target.elements
-    // 💰 console.log(event.target.elements) if you're not sure.
+    event.preventDefault();
+    setQueried(true);
+    setQuery(event.target.elements.search.value);
   }
 
   return (
     <div
-      css={{maxWidth: 800, margin: 'auto', width: '90vw', padding: '40px 0'}}
+      css={{ maxWidth: 800, margin: 'auto', width: '90vw', padding: '40px 0' }}
     >
       <form onSubmit={handleSearchSubmit}>
         <Input
@@ -54,11 +63,24 @@ function DiscoverBooksScreen() {
                 background: 'transparent',
               }}
             >
-              {isLoading ? <Spinner /> : <FaSearch aria-label="search" />}
+              {isLoading ? (
+                <Spinner />
+              ) : isError ? (
+                <FaTimes aria-label="error" css={{color: colors.danger}} />
+              ) : (
+                <FaSearch aria-label="search" />
+              )}
             </button>
           </label>
         </Tooltip>
       </form>
+
+      {isError ? (
+        <div css={{color: colors.danger}}>
+          <p>There was an error:</p>
+          <pre>{error.message}</pre>
+        </div>
+      ) : null}
 
       {isSuccess ? (
         data?.books?.length ? (
@@ -74,7 +96,7 @@ function DiscoverBooksScreen() {
         )
       ) : null}
     </div>
-  )
+  );
 }
 
-export {DiscoverBooksScreen}
+export { DiscoverBooksScreen };
